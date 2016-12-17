82c24cc1bfdde7af47a5892f56ccbe8b82005b86
Chan Chan <nikll123@hotmail.com>
2016-12-17 11:58:52 +0200

    cube +

--- Vector.js
+++ Vector.js
@@ -16,3 +16,41 @@ context.stroke();
 //console.log("begin = " + this.begin.x + ',' + this.begin.y + ";  end = " + this.end.x + ", " + this.end.y );
 
 }
+
+function coordgrids(context)
+{
+      var color = "#000000";
+
+      for (i = -200; i < 200; i = i + 50)
+      {
+        if  (i == 0)
+        {
+          color = "#aaaaff";
+        }
+        else 
+        {
+          color = "#eeeeee";
+        }
+
+          vector = new Vector (new Point(i, -200, 0), new Point(i, 200, 0));
+          vector.color = color;
+          vector.draw(context);
+          vector = new Vector (new Point(-200, i, 0), new Point(200, i, 0));
+          vector.color = color;
+          vector.draw(context);
+
+          vector = new Vector (new Point(i, 0, -200), new Point(i, 0, 200));
+          vector.color = color;
+          vector.draw(context);
+          vector = new Vector (new Point(-200, 0, i), new Point(200, 0, i));
+          vector.color = color;
+          vector.draw(context);
+
+          vector = new Vector (new Point(0, -200, i), new Point(0, 200, i));
+          vector.color = color;
+          vector.draw(context);
+          vector = new Vector (new Point(0, i, -200), new Point(0, i, 200));
+          vector.color = color;
+          vector.draw(context);
+      }
+}
