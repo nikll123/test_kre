cae96e89f7c303e2d922786e11df3933438b9211
Chan Chan <nikll123@hotmail.com>
2016-12-17 11:31:22 +0200

    just corrections
    Signed-off-by: Chan Chan <nikll123@hotmail.com>

--- Vector.js
+++ Vector.js
@@ -2,12 +2,13 @@ function Vector(point1, point2)
 {
   this.begin = point1;
   this.end = point2;
+  this.color = "#000000"; 
 }
 
 Vector.prototype.draw = function(context)
 {
 context.beginPath();
-
+context.strokeStyle = this.color;
 context.moveTo(this.begin.x2d(),this.begin.y2d());
 context.lineTo(this.end.x2d(),this.end.y2d());
 context.stroke();
